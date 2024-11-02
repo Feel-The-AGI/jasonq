fa775e02ef13e11cf6d155d8a800265d03dd8e33
jason <jasonquist.ia@gmail.com>
2024-11-02 16:17:11 +0000

    markdown usage branch

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,26 @@
+const markdownIt = require('markdown-it');
+const markdownItAttrs = require('markdown-it-attrs');
+
 module.exports = function(eleventyConfig) {
+    // Configure Markdown processing
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAttrs);
+
+    eleventyConfig.setLibrary("md", markdownLibrary);
+
+    // Copy assets directory
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
+
     return {
         dir: {
             input: "src",
-            output: "_site",
-            includes: "_includes"  // explicitly specify includes directory
+            output: "docs",
+            includes: "includes"
         }
     };
 }; 
